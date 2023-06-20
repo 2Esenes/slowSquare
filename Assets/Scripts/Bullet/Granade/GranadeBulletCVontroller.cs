28e7518869da51a80e576c44507306423724fad2
2Esenes <104767595+2Esenes@users.noreply.github.com>
2023-06-20 14:20:35 +0300

    wtf

---
+++ Assets/Scripts/Bullet/Granade/GranadeBulletCVontroller.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class GranadeBulletCVontroller : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public CircleCollider2D boxCollider;
+
+    public GameObject _explosionPrefab;
+    private float explosionTime;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        explosionTime = 3;
+    }
+
+    private void Update()
+    {
+        explosionTime -= Time.deltaTime;
+        if (explosionTime < 0)
+        {
+            boxCollider.enabled = true;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.tag == "Enemy")
+        {
+            Instantiate(_explosionPrefab , transform.position, transform.rotation);
+            collision.GetComponent<EnemyBasicController>().Die();
+            Destroy(gameObject);
+        }
+        if (collision.transform.tag == "Player")
+        {
+            Instantiate(_explosionPrefab, transform.position, transform.rotation);
+            collision.GetComponent<PlayerMovement>().Bekle(true);
+            Destroy(gameObject);
+        }
+        if (collision.transform.tag == "Ground")
+        {
+            Instantiate(_explosionPrefab, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+        
+
+    }
+
+
+
+}
--- Assets/Scripts/Player/PlayerFireController.cs
+++ Assets/Scripts/Player/PlayerFireController.cs
@@ -7,9 +7,12 @@ using UnityEngine.UI;
 public class PlayerFireController : MonoBehaviour
 {
     public GameObject BulletPrefab;
+    public GameObject GranadePrefab;
     public Transform firePoint;
     public GameObject fireRotate;
-    public float FireForce = 30;
+    public GameObject ChosingGun;
+
+    public float FireForce = 30;  //granade için 10 ,gun için 30 , machine gun içinde 30  
     
     Vector2 mousePos;
 
@@ -18,7 +21,10 @@ public class PlayerFireController : MonoBehaviour
 
     public GameObject _reloadSlider;
 
-
+    private void Start()
+    {
+        ChosingGun = BulletPrefab;
+    }
     private void Update()
     {
         fireTimer += Time.deltaTime;
@@ -29,7 +35,7 @@ public class PlayerFireController : MonoBehaviour
         float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90;
         fireRotate.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
 
-        if (Input.GetMouseButtonDown(0) && fireTimer >= 1) { Fire(); fireTimer = 0; }
+        if (Input.GetMouseButtonDown(0) && fireTimer >= 1) { Fire(ChosingGun); fireTimer = 0; }
 
         _reloadSlider.GetComponent<Slider>().value = fireTimer;
         if (fireTimer > 1)
@@ -43,9 +49,23 @@ public class PlayerFireController : MonoBehaviour
     
     }
 
-    public void Fire()
+    public void Fire(GameObject gun )
     {
-        GameObject bullet = Instantiate(BulletPrefab, firePoint.position, firePoint.rotation);
+        GameObject bullet = Instantiate(gun, firePoint.position, firePoint.rotation);
         bullet.GetComponent<Rigidbody2D>().AddForce(firePoint.up * FireForce, ForceMode2D.Impulse);
     }
+
+
+    public void MyGun(GameObject guns, float bulletForce)
+    {
+        ChosingGun = guns;
+        FireForce = bulletForce;
+    }
+
+    public void NedenOlmuyon(GameObject _gun)
+    {
+        ChosingGun = _gun;
+    }
+
+
 }
