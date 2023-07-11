81f2305aff7472bb342c8295d92baed6bc24492f
Mesut Engin <mesutengin.3@gmail.com>
2023-07-11 15:02:08 +0300

    push

--- Assets/CrazySDK/CrazyAds/Scripts/CrazyAds.cs
+++ Assets/CrazySDK/CrazyAds/Scripts/CrazyAds.cs
@@ -1,6 +1,7 @@
 ﻿// ReSharper disable once RedundantUsingDirective
 using System;
 // ReSharper disable once RedundantUsingDirective
+using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
